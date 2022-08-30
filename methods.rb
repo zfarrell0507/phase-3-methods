70a8398bb7d1585f9c6fc92577b43979150f2ef0
Zachary Farrell <zfarrell0507@gmail.com>
2022-08-30 18:21:35 -0400

    commit tree b14c97bc84e192fac5998feb0e11f4eb4948af25

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+greet "Jimmy"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default "Naureen"
+
+def add(num1, num2)
+    num1 + num2
+end
+puts add(2, 5)
+
+def halve(int)
+    if int.class != Integer
+        return nil
+    end
+
+    int / 2
+end
+puts halve(6)
